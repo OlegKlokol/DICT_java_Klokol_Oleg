47a274389c51eedb261949e14e384d9f44704773
OlegKlokol <o.v.klokol@student.khai.edu>
2021-12-22 15:58:20 +0200

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Java education");
-=======
-        System.out.println("education");
-        //new string
->>>>>>> 9f6ba57 (Test.java added to feature_branch with change on string #2)
     }
 }
